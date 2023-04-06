fe4963342ca4117c12a61b199986b609b68d6839
Alexandra <alexmiriamd@gmail.com>
2023-04-06 07:58:16 -0600

    nav bar spacing

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -5,7 +5,7 @@ import Form from 'react-bootstrap/Form';
 const SideNav = () =>{
     return(
         //map thru tags and "link" to tag specific product pages
-        <div id = "sideNav">
+        <div id = "sideNav" >
             <Form className="d-flex">
                   <Form.Control
                     type="search"
@@ -14,7 +14,7 @@ const SideNav = () =>{
                     aria-label="Search"
                   />
             </Form>
-          <Nav defaultActiveKey="/home" id="side-bar">
+          <Nav defaultActiveKey="/home" id="side-bar" >
             <Nav.Link href="/home">Fanstasy</Nav.Link>
             <Nav.Link href="link-1">Historical Fiction</Nav.Link>
             <Nav.Link href="link-2">Horror</Nav.Link>
