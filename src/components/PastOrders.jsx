701533fc263297903c5d539d9396ca5507307a10
Thomas <thomasjohnso@gmail.com>
2023-04-07 12:00:33 -0400

    added userprofile, userinfo, past orders components. returning past orders. working on user info

---
+++ src/components/PastOrders.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+const PastOrders = (props) => {
+  const pastOrders = props.pastOrders;
+
+  return (
+    <div>
+      {pastOrders.length ? (
+        pastOrders.map((order, idx) => {
+          return (
+            <Card>
+              <Card.Header>Cart {`${idx + 1}`}</Card.Header>
+              <div>
+                {order.products.map((product, idx) => {
+                  return (
+                    <Card.Body>
+                      <Card.Title>{product.title}</Card.Title>
+                      <Card.Text>Quantity:{product.quantity}</Card.Text>
+                      <Card.Text>Price: ${product.price}</Card.Text>
+                    </Card.Body>
+                  );
+                })}
+              </div>
+              <Card.Footer>Total:</Card.Footer>
+            </Card>
+          );
+        })
+      ) : (
+        <h1>Loading</h1>
+      )}
+    </div>
+  );
+};
+
+export default PastOrders;
