5fea34e28eeb65df9cdd3cdd045d1a9461b94cc0
Thomas <thomasjohnso@gmail.com>
2023-04-08 12:56:52 -0400

    added sideNav to category routes

--- src/components/TaggedProductList.jsx
+++ src/components/TaggedProductList.jsx
@@ -1,42 +1,49 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getProductsByTagCall } from "../API-Adapter";
-import { ProductCard } from "./";
+import { SideNav, ProductCard } from "./";
 
-const TaggedProductList = () => {
-    const { name, tagId } = useParams();
+const TaggedProductList = (props) => {
+  const allTags = props.allTags;
+  const { name, tagId } = useParams();
 
-    const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
-    const getProductsByTag = async () => {
-        const result = await getProductsByTagCall(tagId);
-        if(result.success){
-            setProducts(result.products);
-        } 
+  const getProductsByTag = async () => {
+    const result = await getProductsByTagCall(tagId);
+    if (result.success) {
+      setProducts(result.products);
     }
+  };
 
-    useEffect(() => {
-        getProductsByTag()
-    }, []);
+  useEffect(() => {
+    getProductsByTag();
+  }, []);
 
-    return(
-        <div id="productByTagContainer">
-            <h1 id="productByTagTitle">All {name} Books:</h1>
-           { products.length ? 
-            <div id="productsContainer">
-            {products.length ? (
-              products.map((product, idx) => {
-                return product.isActive ? (
-                  <ProductCard product={product} key={`${idx} - product list map`} />
-                ) : null;
-              })
-            ) : (
-              <h1>LOAFING</h1>
-            )}
-          </div> 
-            : <h1> no products found with this tag! </h1>}
+  return (
+    <div id="productByTagContainer">
+      <h1 id="productByTagTitle">All {name} Books:</h1>
+      {products.length ? (
+        <div id="home-content-container">
+          <SideNav allTags={allTags} />
+          {products.length ? (
+            products.map((product, idx) => {
+              return product.isActive ? (
+                <ProductCard
+                  product={product}
+                  key={`${idx} - product list map`}
+                />
+              ) : null;
+            })
+          ) : (
+            <h1>LOAFING</h1>
+          )}
         </div>
-    )
-}
+      ) : (
+        <h1> no products found with this tag! </h1>
+      )}
+    </div>
+  );
+};
 
 export default TaggedProductList;
