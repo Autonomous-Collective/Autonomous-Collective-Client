23226803b8a12253da1cb114cd9add3a61b2ee75
Josie Parker <josieparker@Josies-MacBook-Pro.local>
2023-05-04 15:33:44 -0500

    started styling product cards and gave body background color

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,9 +3,14 @@ import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
+import productCardStyling from "./componentStyles/productCardStyling.css";
+
 const ProductCard = ({ product }) => {
+
+  const productAuthor = product.author.toUpperCase();
+
   return (
-    <Card style={{ width: "18rem", margin: "10px" }}>
+    <Card style={{ width: "18rem", margin: "10px" }} id="product-card">
       <Card.Img
         variant="top"
         style={{
@@ -15,18 +20,21 @@ const ProductCard = ({ product }) => {
           margin: "auto",
         }}
         src={product.img}
+
+        id="product-card-img"
       />
       <Card.Body>
-        <Card.Title>{product.title}</Card.Title>
-        <Card.Text>{product.author}</Card.Text>
+        <Card.Title id="product-card-title">{product.title}</Card.Title>
+        <Card.Text id="product-card-author">{productAuthor}</Card.Text>
       </Card.Body>
       <Button
         style={{ width: "40%", marginLeft: "30%", marginBottom: "20px" }}
         variant="primary"
         as={Link}
         to={`/products/${product.id}`}
+        id="product-card-button"
       >
-        See More
+        SEE MORE
       </Button>
     </Card>
   );
